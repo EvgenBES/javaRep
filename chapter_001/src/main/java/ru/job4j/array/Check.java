e218abffb9df4647e829f31abaaaf95183a48c52
Evgeniy <jekabut@gmail.com>
2018-07-31 14:36:16 +0300

    6.3.2 Массив заполнен true или false [#53857]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -5,7 +5,7 @@ package ru.job4j.array;
  * Save elements boolean.
  *
  * @author Evgeny Butov (mailto:but87@mail.ru)
- * @version 1.1
+ * @version 1.2
  * @since 31.07.2018
  */
 
@@ -14,26 +14,13 @@ public class Check {
         boolean result = false;
 
         for (int i = 0; i != data.length; i++) {
-            if (data[i] == true) {
+            if (data[0] == data[i]) {
                 result = true;
             } else {
                 result = false;
                 break;
             }
         }
-
-
-        for (int j = 0; j != data.length; j++) {
-            if (result == false) {
-                if (data[j] == false) {
-                    result = true;
-                } else {
-                    result = false;
-                    break;
-                }
-            }
-        }
-
         return result;
     }
 }
