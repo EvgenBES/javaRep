a39bbdd286fbbc01d0401bed3971b4422fb536e5
Evgeniy <jekabut@gmail.com>
2018-08-05 16:59:39 +0300

    refact

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -12,8 +12,8 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
         int poitArray = data.length - 1;
-        for (int i = 0; i != data.length ; i++) {
-            if(data[0][0] !=data[i][i] || data[i][poitArray] != data[poitArray][i]){
+        for (int i = 0; i != data.length; i++) {
+            if (data[0][0] != data[i][i] || data[i][poitArray] != data[poitArray][i]) {
                 result = false;
                 break;
             }
